3265aa9c1916dd426a84a74645223db2802e9c24
Minahil Yasar <minahilyasar@gmail.com>
2025-05-06 16:07:24 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 5 - Starter
+Name: Minahil Yasar
+I am working on this lab alone.
+
+Q1. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+Ans. No, unit testing is most useful for checking small, isolated pieces of logic. The "message" feature involves multiple components so it is too broad to be tested with a unit test. If there's a bug in one specific part, a unit test wouldn't be effective in testing the entire feature.
+
+Q2. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+Ans. Yes unit testing will be helpful as the logic for this singular component is simple. If the message is longer than 80 characters you reject it. You can easily unit test to see if a message is within the acceptable charachter limit or not. 
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,39 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img');
+  const audio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelectorAll('img')[1];
+  const playButton = document.querySelector('button');
+
+  hornSelect.addEventListener('change', () => {
+    const value = hornSelect.value;
+    hornImage.src = `assets/images/${value}.svg`;
+    audio.src = `assets/audio/${value}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = volumeSlider.value;
+    audio.volume = volume / 100;
+
+    if (volume === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    audio.play();
+    if (hornSelect.value === 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
 }
